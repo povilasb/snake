b20700b6686dafcc47e12c03d4b3bfe66a30342d
Povilas Balciunas <balciunas90@gmail.com>
2017-10-05 16:17:36 +0300

    Add function to draw rect

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,8 @@ fn main() {
     let mut canvas = Canvas::new();
     canvas.line((100, 100), (150, 100));
     canvas.line((100, 200), (500, 500));
+    canvas.color(255, 0, 0);
+    canvas.rect((500, 0), (550, 50));
     canvas.draw();
 
     let _ = Framebuffer::set_kd_mode(KdMode::Graphics).unwrap();
@@ -46,15 +48,31 @@ impl Canvas {
         }
     }
 
+    fn color(&mut self, r: u8, g: u8, b: u8) {
+        self.curr_color = (r, g, b)
+    }
+
     fn line(&mut self, from: Point, to: Point) {
         let (m, b) = math::solve_linear_eq(from, to);
         for x in from.0..to.0 {
             let y = (m * x as f64 + b) as u32;
-            let start = (y * self.line_length + x) as usize;
-            self.frame[start]  = self.curr_color.0;
-            self.frame[start + 1]  = self.curr_color.1;
-            self.frame[start + 2]  = self.curr_color.2;
+            self.point(x, y);
         }
+    }
+
+    fn rect(&mut self, left_top: Point, right_bottom: Point) {
+        for y in left_top.1..right_bottom.1 {
+            for x in left_top.0..right_bottom.0 {
+                self.point(x, y);
+            }
+        }
+    }
+
+    fn point(&mut self, x: u32, y: u32) {
+        let start = (y * self.line_length + x * 4) as usize;
+        self.frame[start]  = self.curr_color.2;
+        self.frame[start + 1]  = self.curr_color.1;
+        self.frame[start + 2]  = self.curr_color.0;
     }
 
     fn draw(&mut self) {
