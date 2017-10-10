ed3b34924a67d10d9ad794c9950a587e0170c010
Povilas Balciunas <balciunas90@gmail.com>
2017-10-10 20:35:13 +0300

    Wait for keypress instead of timeout

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,6 @@ use framebuffer::{KdMode, Framebuffer};
 mod math;
 mod gfx;
 
-use std::thread;
-use std::time::Duration;
-
 
 fn main() {
     let mut canvas = gfx::Canvas::new();
@@ -18,6 +15,6 @@ fn main() {
 
     let _ = Framebuffer::set_kd_mode(KdMode::Graphics).unwrap();
     // TODO: Make variable that on destruction gets back to text mode
-    thread::sleep(Duration::from_secs(5));
+    let _ = std::io::stdin().read_line(&mut String::new());
     let _ = Framebuffer::set_kd_mode(KdMode::Text).unwrap();
 }
